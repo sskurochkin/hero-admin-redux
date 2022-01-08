9a486ede3494ff7531435a103b7f62de11844085
sskurochkin <semenkurochkin@gmail.com>
2022-01-08 18:53:53 +0300

    add middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,45 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 // import reducer from '../reducers';
-import heroes from '../reducers/heroes';
-import filters from '../reducers/filters';
+import heroes from "../reducers/heroes";
+import filters from "../reducers/filters";
 
-const enhancer = (createStore) =>(...args) =>  {
-    const store = createStore(...args)
-
-    const oldDispatch = store.oldDispatch // сохраняем оригинальный диспатч
-    store.dispatch = (action) => {
-        if (typeof action === 'string'){
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action)
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === "string") {
+        return next({
+            type: action,
+        });
     }
-    return store
-}
+    return next(action);
+};
+
+const enhancer =
+	(createStore) =>
+	(...args) => {
+		const store = createStore(...args);
+
+		const oldDispatch = store.oldDispatch; // сохраняем оригинальный диспатч
+		store.dispatch = (action) => {
+			if (typeof action === "string") {
+				return oldDispatch({
+					type: action,
+				});
+			}
+			return oldDispatch(action);
+		};
+		return store;
+	};
 
 // const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const store = createStore(
-                        combineReducers({heroes: heroes, filters: filters}),
-                        compose(
-                            enhancer, 
-                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+	combineReducers({ heroes: heroes, filters: filters }),
+        compose(applyMiddleware(stringMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+			window.__REDUX_DEVTOOLS_EXTENSION__()
+	// compose(
+	// 	enhancer,
+	// 	window.__REDUX_DEVTOOLS_EXTENSION__ &&
+	// 		window.__REDUX_DEVTOOLS_EXTENSION__()
+	// )
+));
 
 export default store;
